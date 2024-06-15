3db6243dca346e63e12101c26cab70004e35e30b
kadin <kadinpadams@gmail.com>
2024-06-14 21:52:44 -0600

    created a guess the number game.

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random random = new Random();
+        int number = random.Next(1, 101);
+        int guess = -1;
+
+        while (guess != number)
+        {
+            Console.WriteLine("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (guess < number)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (guess > number)
+            {
+                Console.WriteLine("Lower");
+            }
+
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }      
     }
 }
